37dae86df0f2625e151adfda908f3b11de181996
Mario Castro <mariocaster@gmail.com>
2015-07-05 23:23:57 +0200

    Main file modified to get rid of most non APRS functionality
    Tested and working

--- trackuino/trackuino.ino
+++ trackuino/trackuino.ino
@@ -36,14 +36,9 @@
 // Trackuino custom libs
 #include "config.h"
 #include "afsk_avr.h"
-#include "afsk_pic32.h"
 #include "aprs.h"
-#include "buzzer.h"
-#include "gps.h"
 #include "pin.h"
 #include "power.h"
-#include "sensors_avr.h"
-#include "sensors_pic32.h"
 
 // Arduino/AVR libs
 #if (ARDUINO + 1) >= 100
@@ -69,63 +64,18 @@ void setup()
   Serial.println("RESET");
 #endif
 
-  buzzer_setup();
   afsk_setup();
-  gps_setup();
-  sensors_setup();
 
-#ifdef DEBUG_SENS
-  Serial.print("Ti=");
-  Serial.print(sensors_int_lm60());
-  Serial.print(", Te=");
-  Serial.print(sensors_ext_lm60());
-  Serial.print(", Vin=");
-  Serial.println(sensors_vin());
-#endif
-
-  // Do not start until we get a valid time reference
-  // for slotted transmissions.
-  if (APRS_SLOT >= 0) {
-    do {
-      while (! Serial.available())
-        power_save();
-    } while (! gps_decode(Serial.read()));
-    
-    next_aprs = millis() + 1000 *
-      (APRS_PERIOD - (gps_seconds + APRS_PERIOD - APRS_SLOT) % APRS_PERIOD);
-  }
-  else {
-    next_aprs = millis();
-  }  
-  // TODO: beep while we get a fix, maybe indicating the number of
-  // visible satellites by a series of short beeps?
-}
-
-void get_pos()
-{
-  // Get a valid position from the GPS
-  int valid_pos = 0;
-  uint32_t timeout = millis();
-  do {
-    if (Serial.available())
-      valid_pos = gps_decode(Serial.read());
-  } while ( (millis() - timeout < VALID_POS_TIMEOUT) && ! valid_pos) ;
-
-  if (valid_pos) {
-    if (gps_altitude > BUZZER_ALTITUDE) {
-      buzzer_off();   // In space, no one can hear you buzz
-    } else {
-      buzzer_on();
-    }
-  }
+  next_aprs = millis();
 }
 
 void loop()
 {
   // Time for another APRS frame
   if ((int32_t) (millis() - next_aprs) >= 0) {
-    get_pos();
-    aprs_send();
+    //aprs_send();
+    aprs_send("gps_time", "gps_aprs_lat", "gps_aprs_lon", 100, 101, 102, "int_temperature",
+    "ext_temperature", "sensors_vin");
     next_aprs += APRS_PERIOD * 1000L;
     while (afsk_flush()) {
       power_save();
@@ -137,5 +87,4 @@ void loop()
 #endif
   }
 
-  power_save(); // Incoming GPS data or interrupts will wake us up
 }
